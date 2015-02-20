c25207dcd99fb2c372f3ebbc631c467d90c04f6f
Max Gordon <gordonmaxc@gmail.com>
2015-02-20 14:00:05 -0800

    linked api

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr("string"),
+  username: DS.attr("string"),
+  link: DS.attr("string"),
+  description: DS.attr("string"),
+  comments: DS.hasMany("comment")
 });
