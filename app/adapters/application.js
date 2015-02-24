3e9827651d04815315d7d6fd0d9dccb50339875a
Max Gordon <gordonmaxc@gmail.com>
2015-02-24 14:38:42 -0800

    deployable

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
+import ENV from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-  host: "http://localhost:3000"
+
+  // slight hack until the ember-cli-dotenv package is fixed
+  host: ENV.adapterURL || ENV.ADAPTER_URL
+
 });
