34e8ae03543f99f263038cb371f71efc719998f4
Max Gordon <gordonmaxc@gmail.com>
2015-02-24 15:07:04 -0800

    dotENV

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,5 +5,6 @@ export default DS.ActiveModelAdapter.extend({
 
   // slight hack until the ember-cli-dotenv package is fixed
   host: ENV.adapterURL || ENV.ADAPTER_URL
+  // host: "http://localhost:3000"
 
 });
